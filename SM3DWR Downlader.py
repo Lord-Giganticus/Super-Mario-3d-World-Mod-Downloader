4115dba5e132460b26f20195340a960401a8ad3e
Lord-Giganticus <mrubins64@gmail.com>
2021-01-30 15:42:21 -0600

    added use of curl -L to download the modpack and made some functions. Also renamed the script.

--- SM3DWR Downlader.py
+++ SM3DWR Downlader.py
@@ -9,20 +9,39 @@ drives = win32api.GetLogicalDriveStrings()
 if os.path.isdir("C:\Lord-G") == False:
     os.mkdir("C:\Lord-G")
 os.chdir("C:\Lord-G")
-print("Due to the mod's size, you'll have to dl the latest (and I mean it, the old ones don't work with this.) zip and save it to C:\Lord-G\ as SM3DWR.zip.")
-dl = input("Do you wish to make the script go to the page? If so enter 1. If not just press enter (I hope you have it downloaded if that's true.)")
-if dl == "1":
-    os.system('cmd /c start https://gamebanana.com/gamefiles/download/12437')
+os.system('cmd /c curl -L https://gamebanana.com/dl/513081 > SM3DWR.zip')
 os.system('cmd /c curl http://stahlworks.com/dev/unzip.exe -o unzip.exe')
 os.system('cmd /c unzip *zip')
 os.system('cmd /c del /f *.zip')
 os.system('cmd /c del /f *.exe')
 
 choice = input("There are 2 ways for the mod to be installed. Enter the number corresponding to your choice.\n[1]SDCafiine\n[2]CEMU\n")
+def sdcaffine_place(Region:str):
+    os.chdir(drive_choice)
+    os.chdir('sdcafiine')
+    os.mkdir(Region)
+    os.chdir(Region)
+    os.mkdir("SM3DWR")
+    os.chdir("SM3DWR")
+    sdcafiine_folder = os.getcwd()
+    shutil.copytree("C:\Lord-G\SM3DWR", sdcafiine_folder)
+    input("Complete. Press enter to exit.")
+    exit()
+
+def cemu_place(Region:str):
+    if os.path.isdir(Region) == False:
+        os.mkdir(Region)
+    os.chdir(Region)
+    os.chdir("content")
+    content_folder = os.getcwd()
+    shutil.copytree("C:\Lord-G\SM3DWR", content_folder)
+    input("Complete. Press enter to exit.")
+    exit()
+
 if choice == "1":
-    USA = "0005000010145c00"
-    EUR = "0005000010145d00"
-    JPN = "0005000010106100"
+    USA = str("0005000010145c00")
+    EUR = str("0005000010145d00")
+    JPN = str("0005000010106100")
     print(drives)
     drive_choice = input("Enter the drive that is your SD/USB from above.\n")
     os.chdir(drive_choice)
@@ -37,7 +56,7 @@ if choice == "1":
             exit()
         os.mkdir('wiiu')
         os.chdir('wiiu')
-    if os.path.isdir('apps') and os.path.isdir('apps\SDcafiine') == False:
+    if os.path.isdir('apps') == False:
         os.chdir(drive_choice)
         os.system('cmd /c curl https://www.wiiubru.com/appstore/zips/SDcafiine.zip -o sdcafiine.zip')
         os.system('cmd /c curl http://stahlworks.com/dev/unzip.exe -o unzip.exe')
@@ -48,45 +67,18 @@ if choice == "1":
         os.system('cmd /c del /f *.json')
     region = input("Enter a number corresponding to the region you live in.\n[1]USA\n[2]EUR\[3]JPN\n")
     if region == "1":
-        os.chdir(drive_choice)
-        os.chdir('sdcafiine')
-        os.mkdir(USA)
-        os.chdir(USA)
-        os.mkdir("SM3DWR")
-        os.chdir("SM3DWR")
-        sdcafiine_folder = os.getcwd()
-        shutil.copytree("C:\Lord-G\SM3DWR", sdcafiine_folder)
-        input("Complete. Press enter to exit.")
-        exit()
+        sdcaffine_place(USA)
     elif region == "2":
-        os.chdir(drive_choice)
-        os.chdir('sdcafiine')
-        os.mkdir(EUR)
-        os.chdir(EUR)
-        os.mkdir("SM3DWR")
-        os.chdir("SM3DWR")
-        sdcafiine_folder = os.getcwd()
-        shutil.copytree("C:\Lord-G\SM3DWR", sdcafiine_folder)
-        input("Complete. Press enter to exit.")
-        exit()
+        sdcaffine_place(EUR)
     elif region == "3":
-        os.chdir(drive_choice)
-        os.chdir('sdcafiine')
-        os.mkdir(JPN)
-        os.chdir(JPN)
-        os.mkdir("SM3DWR")
-        os.chdir("SM3DWR")
-        sdcafiine_folder = os.getcwd()
-        shutil.copytree("C:\Lord-G\SM3DWR", sdcafiine_folder)
-        input("Complete. Press enter to exit.")
-        exit()
+        sdcaffine_place(JPN)
     else:
         input("Well, you chose wrong! You'll have to rerun the program so press enter to exit.")
         exit()
 elif choice == "2":
-    USA = "10145c00"
-    EUR = "10145d00"
-    JPN = "10106100"
+    USA = str("10145c00")
+    EUR = str("10145d00")
+    JPN = str("10106100")
     mlc01_location = input("Enter the path to your cemu's mlc01 folder:\n")
     os.chdir(mlc01_location)
     os.chdir("usr")
@@ -94,32 +86,11 @@ elif choice == "2":
     os.chdir("00050000")
     region = input("Enter a number corresponding to the region you live in.\n[1]USA\n[2]EUR\[3]JPN\n")
     if region == "1":
-        if os.path.isdir(USA) == False:
-            os.mkdir(USA)
-        os.chdir(USA)
-        os.chdir("content")
-        content_folder = os.getcwd()
-        shutil.copytree("C:\Lord-G\SM3DWR", content_folder)
-        input("Complete. Press enter to exit.")
-        exit()
+        cemu_place(USA)
     elif region == "2":
-        if os.path.isdir(EUR) == False:
-            os.mkdir(EUR)
-        os.chdir(EUR)
-        os.chdir("content")
-        content_folder = os.getcwd()
-        shutil.copytree("C:\Lord-G\SM3DWR", content_folder)
-        input("Complete. Press enter to exit.")
-        exit()
+        cemu_place(EUR)
     elif region == "3":
-        if os.path.isdir(JPN) == False:
-            os.mkdir(JPN)
-        os.chdir(JPN)
-        os.chdir("content")
-        content_folder = os.getcwd()
-        shutil.copytree("C:\Lord-G\SM3DWR", content_folder)
-        input("Complete. Press enter to exit.")
-        exit()
+        cemu_place(JPN)
     else:
         input("Well, you chose wrong! You'll have to rerun the program so press enter to exit.")
         exit()
