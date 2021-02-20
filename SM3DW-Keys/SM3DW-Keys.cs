bfe2abb91ffa940b0138a8cdb1f46553b0353769
Lord-Giganticus <mrubins64@gmail.com>
2021-02-20 10:12:23 -0600

    Finish USA option.

--- SM3DW-Keys/SM3DW-Keys.cs
+++ SM3DW-Keys/SM3DW-Keys.cs
@@ -2,11 +2,11 @@
 {
     public static class USA
     {
-        public static string key = "0005000010145c00";
+        public static string key = "0005000010145C00";
     }
     public static class EUR
     {
-        public static string key = "0005000010145d00";
+        public static string key = "0005000010145D00";
     }
     public static class JPN
     {
--- Super-Mario-3d-World-Mod-Downloader/Main.cs
+++ Super-Mario-3d-World-Mod-Downloader/Main.cs
@@ -2,21 +2,19 @@
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
+using System.Threading;
 using SM3DW_Keys;
 
 namespace Super_Mario_3d_World_Mod_Downloader
 {
     class Program
     {
-        public string USA_Key = USA.key;
-        public string EUR_Key = EUR.key;
-        public string JPN_Key = JPN.key;
         static void Main()
         {
             Console.WriteLine("Enter the 3DW Gamebanana download link you want to install for:");
             string link = Console.ReadLine();
             Environment.CurrentDirectory = Directory.GetCurrentDirectory();
-            Process.Start("CMD.exe","curl -L "+link+" -o output.zip").WaitForExit();
+            Process.Start("CMD.exe","curl -L "+link+" -o output.zip && exit").WaitForExit();
             Directory.CreateDirectory("Copy");
             ZipFile.ExtractToDirectory("output.zip", "Copy");
             File.Delete("output.zip");
@@ -42,15 +40,41 @@ namespace Super_Mario_3d_World_Mod_Downloader
             Console.WriteLine("Enter the drive letter of your SD card;");
             string drive = Console.ReadLine();
             string cwd = Directory.GetCurrentDirectory();
-            Directory.SetCurrentDirectory(drive);
-            if (Directory.Exists("sdcafiine") == true)
+            Directory.SetCurrentDirectory("Copy");
+            string[] dirs = Directory.GetDirectories(Directory.GetCurrentDirectory());
+            foreach (string folder in dirs)
             {
-                Directory.SetCurrentDirectory("sdcafiine");
-            } else
-            {
-                Directory.CreateDirectory("sdcaffine");
-                Directory.SetCurrentDirectory("sdcafiine");
+                if (Directory.Exists(folder) == true)
+                {
+                    Directory.SetCurrentDirectory(folder);
+                    if (folder.EndsWith("content") == true)
+                    {
+                        goto content_folder_found;
+                    } else
+                    {
+                        //pass
+                    }
+                } else
+                {
+                    //pass
+                }
             }
+        content_folder_found:
+            string content_folder = Directory.GetCurrentDirectory();
+            if (choice == "1")
+            {
+                Process.Start("CMD.exe", "robocopy \"" + content_folder + "\" \"" + drive + @"\sdcafiine\" + USA.key + @"\ModPack1\" + "content\" && exit").WaitForExit();
+            } else if (choice == "2")
+            {
+                Process.Start("CMD.exe", "robocopy \"" + content_folder + "\" \"" + drive + @"\sdcafiine\" + EUR.key + @"\ModPack1\" + "content\" && exit").WaitForExit();
+            } else if (choice == "3")
+            {
+                Process.Start("CMD.exe", "robocopy \"" + content_folder + "\" \"" + drive + @"\sdcafiine\" + JPN.key + @"\ModPack1\" + "content\" && exit").WaitForExit();
+            }
+            goto complete;
+        complete:
+            Console.WriteLine("Complete. Exiting.");
+            Thread.Sleep(3000);
         }
     }
 }
