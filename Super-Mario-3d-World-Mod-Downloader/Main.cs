eb0b65cd9587b6f7167d4c4ede6ed9927c0ad31d
Lord-Giganticus <mrubins64@gmail.com>
2021-02-19 22:04:55 -0600

    Update code

--- Super-Mario-3d-World-Mod-Downloader/Main.cs
+++ Super-Mario-3d-World-Mod-Downloader/Main.cs
@@ -16,9 +16,41 @@ namespace Super_Mario_3d_World_Mod_Downloader
             Console.WriteLine("Enter the 3DW Gamebanana download link you want to install for:");
             string link = Console.ReadLine();
             Environment.CurrentDirectory = Directory.GetCurrentDirectory();
-            Process.Start("CMD.exe","curl -L "+link+" -o output.zip");
-            ZipFile.ExtractToDirectory("output.zip", Directory.GetCurrentDirectory());
+            Process.Start("CMD.exe","curl -L "+link+" -o output.zip").WaitForExit();
+            Directory.CreateDirectory("Copy");
+            ZipFile.ExtractToDirectory("output.zip", "Copy");
             File.Delete("output.zip");
+            Console.WriteLine("Enter the region you want to install for.\n[1] USA\n[2] EUR\n[3] JPN\n");
+            string choice = Console.ReadLine();
+            if (choice == "1")
+            {
+                goto USA;
+            }
+            else if (choice == "2")
+            {
+                //pass for now
+            }
+            else if (choice == "3")
+            {
+                //pass for now
+            }
+            else
+            {
+                Environment.Exit(1);
+            }
+        USA:
+            Console.WriteLine("Enter the drive letter of your SD card;");
+            string drive = Console.ReadLine();
+            string cwd = Directory.GetCurrentDirectory();
+            Directory.SetCurrentDirectory(drive);
+            if (Directory.Exists("sdcafiine") == true)
+            {
+                Directory.SetCurrentDirectory("sdcafiine");
+            } else
+            {
+                Directory.CreateDirectory("sdcaffine");
+                Directory.SetCurrentDirectory("sdcafiine")
+            }
         }
     }
 }
