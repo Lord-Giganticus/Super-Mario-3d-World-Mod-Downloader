18ebd614ea9f145d04de1709f45f308ed621cc5c
Lord-Giganticus <mrubins64@gmail.com>
2021-02-20 20:33:04 -0600

    change cmd calling

--- copy.cmd
+++ copy.cmd
@@ -1,4 +1,2 @@
-msbuild SM3DW-Keys\SM3DW-Keys.csproj -p:Configuration=Release
-msbuild Super-Mario-3d-World-Mod-Downloader.sln -p:Configuration=Release
 copy Super-Mario-3d-World-Mod-Downloader\bin\Release\Super-Mario-3d-World-Mod-Downloader.exe %CD%
 copy Super-Mario-3d-World-Mod-Downloader\bin\Release\SM3DW-Keys.dll %CD%
