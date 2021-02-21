1f9b91d1df3d57e8e6be6de0710f15e940f3c6a7
Lord-Giganticus <mrubins64@gmail.com>
2021-02-20 19:36:39 -0600

    Update Build Script

--- Build_Script.cmd
+++ Build_Script.cmd
@@ -1,5 +1,4 @@
 msbuild SM3DW-Keys\SM3DW-Keys.csproj -p:Configuration=Release
 msbuild Super-Mario-3d-World-Mod-Downloader.sln -p:Configuration=Release
-move Super-Mario-3d-World-Mod-Downloader\bin\Release\Super-Mario-3d-World-Mod-Downloader.exe %CD%
-move Super-Mario-3d-World-Mod-Downloader\bin\Release\SM3DW-Keys.dll %CD%
-7z a Release.zip Super-Mario-3d-World-Mod-Downloader.exe SM3DW-Keys.dll
+copy Super-Mario-3d-World-Mod-Downloader\bin\Release\Super-Mario-3d-World-Mod-Downloader.exe %CD%
+copy Super-Mario-3d-World-Mod-Downloader\bin\Release\SM3DW-Keys.dll %CD%
